50be2ff58234a54bfdd55ce2c1f05ab29a13c385
yusufakbs <akbasyusuuf@gmail.com>
2024-04-19 15:15:18 +0300

    SQL query notes

---
+++ dbQueries/sql_queries/Untitled.sql
@@ -0,0 +1,10 @@
+CREATE TABLE cars(
+	brand VARCHAR(255), 
+	model VARCHAR(255),
+	year INT
+);
+
+INSERT INTO cars(brand,model,year) VALUES
+('Astron Martin','DB 11',2013),
+('BMW','M4',2023),
+('Cobra','Shelby',1962);
--- src/stream/question/Main.java
+++ src/stream/question/Main.java
@@ -1,16 +1,34 @@
 package stream.question;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
+import java.util.function.BiConsumer;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 
 public class Main {
     public static void main(String[] args) {
-        List<Student> studentList = createStudents(1000000);
+        List<Student> studentList = createStudents(100);
         studentList.forEach(System.out::println);
         double studentGradeAverage = studentList.stream().mapToDouble(Student::getGrade).average().orElse(0);
         System.out.println(studentGradeAverage);
+
+        List<Student> studentBellowBound = studentList.stream()
+                .filter(x -> x.getGrade() < 50).toList();
+
+        List<Student> studentList1 = studentList;
+        studentList1.forEach(student -> System.out.println("Name:" + student.getName() + ", Surname:" + student.getSurname()));
+
+
+        Map<String, Integer> newList = new HashMap<>();
+        newList = studentList.stream().collect(Collectors.toMap(k -> k.getSurname(), Student::getGrade));
+        System.out.println(newList.values());
+
+
+
+
+
+
 
     }
 
