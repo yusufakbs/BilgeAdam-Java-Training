eb62e334f6115fd0373cda32e14a2b10a9dcf3ca
yusufakbs <akbasyusuuf@gmail.com>
2024-05-02 12:32:47 +0300

    Spring project created

---
+++ KJ_SpringTest/src/main/java/com/yusuf/KjSpringTestApplication.java
@@ -0,0 +1,13 @@
+package com.yusuf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KjSpringTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KjSpringTestApplication.class, args);
+	}
+
+}
